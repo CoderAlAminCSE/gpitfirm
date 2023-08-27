e42837a2da27a9086b6fd916962b6c3c39f865dc
CoderAlAminCSE <alaminislam858542@gmail.com>
2023-08-27 18:01:48 +0600

    toaster added when invoice link copied

--- public/assets/backend/js/invoice.js
+++ public/assets/backend/js/invoice.js
@@ -1,14 +1,32 @@
 function copyLink(button) {
-  const linkToCopy = button.getAttribute("data-link");
-  const currentURL = window.location.origin; // Get the current website's URL
-  const fullLinkToCopy = currentURL + "/invoice/" + linkToCopy;
+    const linkToCopy = button.getAttribute("data-link");
+    const currentURL = window.location.origin; // Get the current website's URL
+    const fullLinkToCopy = currentURL + "/invoice/" + linkToCopy;
 
-  const tempInput = document.createElement("input");
-  tempInput.value = fullLinkToCopy;
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand("copy");
-  document.body.removeChild(tempInput);
+    const tempInput = document.createElement("input");
+    tempInput.value = fullLinkToCopy;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
 
-  alert("Link copied to clipboard!");
+    toastr.options = {
+        closeButton: false,
+        debug: false,
+        newestOnTop: false,
+        progressBar: false,
+        positionClass: "toastr-top-right",
+        preventDuplicates: false,
+        onclick: null,
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut",
+    };
+
+    toastr.success("Invoice link copied successfully");
 }

0e5f04089f9ed1c8c7eb21f931d1933562dfb306
CoderAlAminCSE <alaminislam858542@gmail.com>
2023-09-03 18:03:27 +0600

    admin dashboard home page dynamic

--- resources/views/backend/home/home.blade.php
+++ resources/views/backend/home/home.blade.php
@@ -26,7 +26,7 @@
         </div>
         <!--end::Toolbar-->
         <!--begin::Content-->
-        <div id="kt_app_content" class="app-content flex-column-fluid">
+        {{-- <div id="kt_app_content" class="app-content flex-column-fluid">
             <!--begin::Content container-->
             <div id="kt_app_content_container" class="app-container container-fluid">
                 <!--begin::Row-->
@@ -72,225 +72,97 @@
                         <!--end::Card widget 20-->
                     </div>
                     <!--end::Col-->
+                </div>
+                <!--end::Row-->
+            </div>
+            <!--end::Content container-->
+        </div> --}}
+
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <!--begin::Content container-->
+            <div id="kt_app_content_container" class="app-container container-fluid">
+                <!--begin::Row-->
+                <div class="row g-5 gx-xl-10 mb-5 mb-xl-10">
                     <!--begin::Col-->
-                    <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10">
-                        <!--begin::Card widget 17-->
-                        <div class="card card-flush h-md-50 mb-5 mb-xl-10">
+                    <div class="col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-md-5 mb-xl-10">
+                        <!--begin::Card widget 20-->
+                        <div class="card card-flush bgi-no-repeat bgi-size-contain bgi-position-x-end mb-5 mb-xl-10"
+                            style="background-color: #F1416C;background-image:url('{{ asset('assets/backend') }}/media/patterns/vector-1.png')">
                             <!--begin::Header-->
-                            <div class="card-header pt-5">
+                            <div class="card-header pt-8 pb-8"> <!-- Increased top and bottom padding -->
                                 <!--begin::Title-->
                                 <div class="card-title d-flex flex-column">
-                                    <!--begin::Info-->
-                                    <div class="d-flex align-items-center">
-                                        <!--begin::Currency-->
-                                        <span class="fs-4 fw-semibold text-gray-400 me-1 align-self-start">$</span>
-                                        <!--end::Currency-->
-                                        <!--begin::Amount-->
-                                        <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">69,700</span>
-                                        <!--end::Amount-->
-                                        <!--begin::Badge-->
-                                        <span class="badge badge-light-success fs-base">
-                                            <i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
-                                                <span class="path1"></span>
-                                                <span class="path2"></span>
-                                            </i>2.2%</span>
-                                        <!--end::Badge-->
-                                    </div>
-                                    <!--end::Info-->
+                                    <!--begin::Amount-->
+                                    <span
+                                        class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{ activeServices()->count() }}</span>
+                                    <!--end::Amount-->
                                     <!--begin::Subtitle-->
-                                    <span class="text-gray-400 pt-1 fw-semibold fs-6">Projects Earnings
-                                        in April</span>
+                                    <span class="text-white opacity-75 pt-3 fw-semibold fs-6 mb-5">Active Services</span>
+                                    <!-- Increased top and bottom padding -->
                                     <!--end::Subtitle-->
                                 </div>
                                 <!--end::Title-->
                             </div>
-                            <!--end::Header-->
-                            <!--begin::Card body-->
-                            <div class="card-body pt-2 pb-4 d-flex flex-wrap align-items-center">
-                                <!--begin::Chart-->
-                                <div class="d-flex flex-center me-5 pt-2">
-                                    <div id="kt_card_widget_17_chart" style="min-width: 70px; min-height: 70px"
-                                        data-kt-size="70" data-kt-line="11"></div>
-                                </div>
-                                <!--end::Chart-->
-                                <!--begin::Labels-->
-                                <div class="d-flex flex-column content-justify-center flex-row-fluid">
-                                    <!--begin::Label-->
-                                    <div class="d-flex fw-semibold align-items-center">
-                                        <!--begin::Bullet-->
-                                        <div class="bullet w-8px h-3px rounded-2 bg-success me-3">
-                                        </div>
-                                        <!--end::Bullet-->
-                                        <!--begin::Label-->
-                                        <div class="text-gray-500 flex-grow-1 me-4">Leaf CRM</div>
-                                        <!--end::Label-->
-                                        <!--begin::Stats-->
-                                        <div class="fw-bolder text-gray-700 text-xxl-end">$7,660</div>
-                                        <!--end::Stats-->
-                                    </div>
-                                    <!--end::Label-->
-                                    <!--begin::Label-->
-                                    <div class="d-flex fw-semibold align-items-center my-3">
-                                        <!--begin::Bullet-->
-                                        <div class="bullet w-8px h-3px rounded-2 bg-primary me-3">
-                                        </div>
-                                        <!--end::Bullet-->
-                                        <!--begin::Label-->
-                                        <div class="text-gray-500 flex-grow-1 me-4">Mivy App</div>
-                                        <!--end::Label-->
-                                        <!--begin::Stats-->
-                                        <div class="fw-bolder text-gray-700 text-xxl-end">$2,820</div>
-                                        <!--end::Stats-->
-                                    </div>
-                                    <!--end::Label-->
-                                    <!--begin::Label-->
-                                    <div class="d-flex fw-semibold align-items-center">
-                                        <!--begin::Bullet-->
-                                        <div class="bullet w-8px h-3px rounded-2 me-3" style="background-color: #E4E6EF">
-                                        </div>
-                                        <!--end::Bullet-->
-                                        <!--begin::Label-->
-                                        <div class="text-gray-500 flex-grow-1 me-4">Others</div>
-                                        <!--end::Label-->
-                                        <!--begin::Stats-->
-                                        <div class="fw-bolder text-gray-700 text-xxl-end">$45,257</div>
-                                        <!--end::Stats-->
-                                    </div>
-                                    <!--end::Label-->
-                                </div>
-                                <!--end::Labels-->
-                            </div>
-                            <!--end::Card body-->
                         </div>
-                        <!--end::Card widget 17-->
-                        <!--begin::List widget 26-->
-                        <div class="card card-flush h-lg-50">
-                            <!--begin::Header-->
-                            <div class="card-header pt-5">
-                                <!--begin::Title-->
-                                <h3 class="card-title text-gray-800 fw-bold">External Links</h3>
-                                <!--end::Title-->
-                                <!--begin::Toolbar-->
-                                <div class="card-toolbar">
-                                    <!--begin::Menu-->
-                                    <button
-                                        class="btn btn-icon btn-color-gray-400 btn-active-color-primary justify-content-end"
-                                        data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end"
-                                        data-kt-menu-overflow="true">
-                                        <i class="ki-duotone ki-dots-square fs-1 text-gray-400 me-n1">
-                                            <span class="path1"></span>
-                                            <span class="path2"></span>
-                                            <span class="path3"></span>
-                                            <span class="path4"></span>
-                                        </i>
-                                    </button>
-                                    <!--begin::Menu 2-->
-                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px"
-                                        data-kt-menu="true">
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3">
-                                            <div class="menu-content fs-6 text-dark fw-bold px-3 py-4">
-                                                Quick Actions</div>
-                                        </div>
-                                        <!--end::Menu item-->
-                                        <!--begin::Menu separator-->
-                                        <div class="separator mb-3 opacity-75"></div>
-                                        <!--end::Menu separator-->
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3">
-                                            <a href="#" class="menu-link px-3">New Ticket</a>
-                                        </div>
-                                        <!--end::Menu item-->
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3">
-                                            <a href="#" class="menu-link px-3">New Customer</a>
-                                        </div>
-                                        <!--end::Menu item-->
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3" data-kt-menu-trigger="hover"
-                                            data-kt-menu-placement="right-start">
-                                            <!--begin::Menu item-->
-                                            <a href="#" class="menu-link px-3">
-                                                <span class="menu-title">New Group</span>
-                                                <span class="menu-arrow"></span>
-                                            </a>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu sub-->
-                                            <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3">
-                                                    <a href="#" class="menu-link px-3">Admin
-                                                        Group</a>
-                                                </div>
-                                                <!--end::Menu item-->
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3">
-                                                    <a href="#" class="menu-link px-3">Staff
-                                                        Group</a>
-                                                </div>
-                                                <!--end::Menu item-->
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3">
-                                                    <a href="#" class="menu-link px-3">Member
-                                                        Group</a>
-                                                </div>
-                                                <!--end::Menu item-->
-                                            </div>
-                                            <!--end::Menu sub-->
-                                        </div>
-                                        <!--end::Menu item-->
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3">
-                                            <a href="#" class="menu-link px-3">New Contact</a>
-                                        </div>
-                                        <!--end::Menu item-->
-                                        <!--begin::Menu separator-->
-                                        <div class="separator mt-3 opacity-75"></div>
-                                        <!--end::Menu separator-->
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3">
-                                            <div class="menu-content px-3 py-3">
-                                                <a class="btn btn-primary btn-sm px-4" href="#">Generate Reports</a>
-                                            </div>
-                                        </div>
-                                        <!--end::Menu item-->
-                                    </div>
-                                    <!--end::Menu 2-->
-                                    <!--end::Menu-->
-                                </div>
-                                <!--end::Toolbar-->
-                            </div>
-                            <!--end::Header-->
-                            <!--begin::Body-->
-                            <div class="card-body pt-5">
-                                <!--begin::Item-->
-                                <div class="d-flex flex-stack">
-                                    <!--begin::Section-->
-                                    <a href="#" class="text-primary fw-semibold fs-6 me-2">Avg.
-                                        Client Rating</a>
-                                    <!--end::Section-->
-                                    <!--begin::Action-->
-                                    <button type="button"
-                                        class="btn btn-icon btn-sm h-auto btn-color-gray-400 btn-active-color-primary justify-content-end">
-                                        <i class="ki-duotone ki-exit-right-corner fs-2">
-                                            <span class="path1"></span>
-                                            <span class="path2"></span>
-                                        </i>
-                                    </button>
-                                    <!--end::Action-->
-                                </div>
-                                <!--end::Item-->
-                            </div>
-                            <!--end::Body-->
-                        </div>
-                        <!--end::LIst widget 26-->
+                        <!--end::Card widget 20-->
                     </div>
-                    <!--end::Col-->
+
+
+
+                    <!--begin::Col-->
+                    <div class="col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-md-5 mb-xl-10">
+                        <!--begin::Card widget 20-->
+                        <div class="card card-flush bgi-no-repeat bgi-size-contain bgi-position-x-end mb-5 mb-xl-10"
+                            style="background-color: #F1416C;background-image:url('{{ asset('assets/backend') }}/media/patterns/vector-1.png')">
+                            <!--begin::Header-->
+                            <div class="card-header pt-8 pb-8"> <!-- Increased top and bottom padding -->
+                                <!--begin::Title-->
+                                <div class="card-title d-flex flex-column">
+                                    <!--begin::Amount-->
+                                    <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{ orders()->count() }}</span>
+                                    <!--end::Amount-->
+                                    <!--begin::Subtitle-->
+                                    <span class="text-white opacity-75 pt-3 fw-semibold fs-6 mb-5">Total Order</span>
+                                    <!-- Increased top and bottom padding -->
+                                    <!--end::Subtitle-->
+                                </div>
+                                <!--end::Title-->
+                            </div>
+                        </div>
+                        <!--end::Card widget 20-->
+                    </div>
+
+                    <!--begin::Col-->
+                    <div class="col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-md-5 mb-xl-10">
+                        <!--begin::Card widget 20-->
+                        <div class="card card-flush bgi-no-repeat bgi-size-contain bgi-position-x-end mb-5 mb-xl-10"
+                            style="background-color: #F1416C;background-image:url('{{ asset('assets/backend') }}/media/patterns/vector-1.png')">
+                            <!--begin::Header-->
+                            <div class="card-header pt-8 pb-8"> <!-- Increased top and bottom padding -->
+                                <!--begin::Title-->
+                                <div class="card-title d-flex flex-column">
+                                    <!--begin::Amount-->
+                                    <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{ invoices()->count() }}</span>
+                                    <!--end::Amount-->
+                                    <!--begin::Subtitle-->
+                                    <span class="text-white opacity-75 pt-3 fw-semibold fs-6 mb-5">Total Invoices</span>
+                                    <!-- Increased top and bottom padding -->
+                                    <!--end::Subtitle-->
+                                </div>
+                                <!--end::Title-->
+                            </div>
+                        </div>
+                        <!--end::Card widget 20-->
+                    </div>
                 </div>
                 <!--end::Row-->
             </div>
             <!--end::Content container-->
         </div>
+
+
+
+
         <!--end::Content-->
     </div>
 @endsection

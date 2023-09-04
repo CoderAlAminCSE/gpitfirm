b7a4a69b065dee2f5dd5f90f70271c1f55320342
CoderAlAminCSE <alaminislam858542@gmail.com>
2023-09-04 13:20:35 +0600

    newsletter subscriber lst table design fix

--- resources/views/backend/newsletter/index.blade.php
+++ resources/views/backend/newsletter/index.blade.php
@@ -163,12 +163,12 @@
                             <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users">
                                 <thead>
                                     <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
-                                        <th class="w-10px pe-2">
-                                            {{-- <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
+                                        {{-- <th class="w-10px pe-2">
+                                            <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
                                                 <input class="form-check-input" type="checkbox" data-kt-check="true"
                                                     value="1" />
-                                            </div> --}}
-                                        </th>
+                                            </div>
+                                        </th> --}}
                                         <th style="width: 20%;">SL</th>
                                         <th style="width: 60%;">Email</th>
                                         <th style="width: 20%;">Actions</th>
